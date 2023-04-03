6417e1b93ea2c7ee8c84123c4e65b4c5204b851a
vugonz <82618983+vugonz@users.noreply.github.com>
2023-04-03 13:55:04 +0100

    add encode.py

--- src/bencode/__init__.py
+++ src/bencode/__init__.py
@@ -1,13 +1,25 @@
 from .decode import Decoder
+from .encode import Encoder
 
-def loads(bencoded_str: bytes) -> dict:
-    return Decoder(bencoded_str).decode()
+def loads(s: bytes) -> dict:
+    """Deserialize ``s`` (``bytes`` or ``bytearray`` instance
+    containing a Bencoded document) to a Python object.
+    """
+    return Decoder(s).decode()
 
 def load(fp) -> dict:
+    """Deserialize ``fp`` (a ``.read()``-supporting file-like object containing
+    a Bencoded document) to a Python object.
+    """
     return Decoder(fp.read()).decode()
 
-def dumps():
-    ...
+def dumps(obj):
+    """Serialize ``obj`` to a JSON formatted ``str``."""
+    return Encoder().encode(obj)
 
-def dump():
-    ...
+def dump(obj, fp):
+    """Serialize ``obj`` as a Bencoded formatted stream to ``fp`` (a
+    ``.write()``-supporting file-like object).
+    """
+
+    fp.write(Encoder().encode(obj))
---
+++ src/bencode/encode.py
@@ -0,0 +1,75 @@
+class BencodeEncodingError(ValueError):
+    ...
+
+class Encoder:
+    """Simple Bencode encoder <https://en.wikipedia.org/wiki/Bencode>
+    
+    Performs the following translations in encoding by default:
+    +---------------+-------------------+
+    | Python        | Bencode           |
+    +===============+===================+
+    | int           | integer           |
+    +---------------+-------------------+
+    | bytes         | string            |
+    +---------------+-------------------+
+    | list          | list              |
+    +---------------+-------------------+
+    | dict          | dictionary        |
+    +---------------+-------------------+
+    """
+
+
+    def __init__(self):
+        # No need for state (yet TM)
+        ...
+
+    @classmethod
+    def encode(cls, item) -> bytes:
+        """ Encode passed item into corresponding Bencode type """
+        if isinstance(item, dict):
+            ret = cls.encode_dict(item)
+        elif isinstance(item, list):
+            ret = cls.encode_list(item)
+        elif isinstance(item, int):
+            ret = cls.encode_int(item)
+        elif isinstance(item, bytes):
+            ret = cls.encode_str(item)
+        else:
+            raise BencodeEncodingError(f"Unsupported type {type(item)}")
+
+        return ret
+
+    @classmethod
+    def encode_int(cls, num: int) -> bytes:
+        res = bytearray(b'i') 
+        res += bytes(str(num), "utf-8")
+        res += b'e'
+
+        return bytes(res)
+
+    @classmethod
+    def encode_str(cls, string: str) -> bytes:
+        res = bytearray(bytes(str(len(string)), 'utf-8'))
+        res += b':'
+        res += string
+
+        return bytes(res)
+
+    @classmethod
+    def encode_list(cls, lst: list) -> bytes:
+        res = bytearray(b'l')
+        for item in lst:
+            res += cls.encode(item)
+        res += b'e'
+
+        return bytes(res)
+
+    @classmethod
+    def encode_dict(cls, dic: dict) -> bytes:
+        res = bytearray(b'd')
+        for k, v in dic.items():
+            res += cls.encode(k)
+            res += cls.encode(v)
+        res += b'e'
+
+        return bytes(res)
