0e5b69d2bad588879f50c8dc711130faebee4aa9
vugonz <goncalooazevedo@outlook.com>
2023-08-20 19:51:18 +0100

    convert meta-info keys to strings instead of bytestrings

---
+++ build/lib/bencode/__init__.py
@@ -0,0 +1,27 @@
+from .decode import Decoder
+from .encode import Encoder
+
+def loads(s):
+    """Deserialize ``s`` (``bytes`` or ``bytearray`` instance
+    containing a Bencoded document) to a Python object.
+    """
+    return Decoder(s).decode()
+
+def load(fp):
+    """Deserialize ``fp`` (a ``.read()``-supporting file-like object containing
+    a Bencoded document) to a Python object.
+    """
+    return Decoder(fp.read()).decode()
+
+def dumps(obj):
+    """Serialize ``obj`` to a JSON formatted ``str``."""
+    return Encoder().encode(obj)
+
+def dump(obj, fp):
+    """Serialize ``obj`` as a Bencoded formatted stream to ``fp`` (a
+    ``.write()``-supporting file-like object).
+    """
+    fp.write(Encoder().encode(obj))
+
+
+
